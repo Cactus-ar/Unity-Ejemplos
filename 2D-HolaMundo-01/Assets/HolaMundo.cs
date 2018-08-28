de4a36f9ce757bb60d8e89974600cbbc98517a74
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2018-08-28 07:47:55 -0300

    Commit Inicial
    Basicamente un test para ver si las dos herramientas trabajan correctamente

---
+++ 2D-HolaMundo-01/Assets/HolaMundo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("Hola Mundo desde Visual Studio!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
