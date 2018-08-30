48ff16f12d5c33573a37b42aed1629afb35b931d
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2018-08-30 16:52:33 -0300

    Ejemplo 3
    Texto y Canvas

---
+++ 2D-Texto-04/Assets/JuegoAventura.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JuegoAventura : MonoBehaviour {
+
+    // Use this for initialization
+
+    [SerializeField] Text componenteTexto;
+
+	void Start () {
+        componenteTexto.text = ("Aquí va el texto de la historia");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
