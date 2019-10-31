9963b81ff484cc34aad91494bfc0c435c4fb5f8c
Gabriel Ceravolo <Cactus-ar@users.noreply.github.com>
2019-10-31 08:08:27 -0300

    Sistema de Menu inicial y Opciones

---
+++ Menu/Assets/MenuInicial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void CargarJuego()
+    {
+        //Va a cargar la siguiente escena configurada en las Build Settings
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   public void Salir()
+    {
+        Application.Quit();
+    }
+}
